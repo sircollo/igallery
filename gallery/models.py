5c7ac632de435366165c2b5e9148f16dc6c2d15f
Collins Sirwani <sircollo71@gmail.com>
2022-05-28 23:39:28 +0300

    Feat: adds location and category model class

--- gallery/models.py
+++ gallery/models.py
@@ -2,14 +2,30 @@ from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
-
-class Image(models.Model):
+class Location(models.Model):
   name = models.CharField(max_length=30)
-  description = models.TextField()
-  picture = models.ImageField(upload_to='images',default='image')
-  date = models.DateTimeField(auto_now_add=True)
   
   def __str__(self):
     return self.name
+  
+class Category(models.Model):
+  name = models.CharField(max_length=30)
+  
+  def __str__(self):
+    return self.name
+  
+class Image(models.Model):
+  name = models.CharField(max_length=30)
+  description = models.TextField()
+  picture = models.ImageField(upload_to='images/',default='image')
+  date = models.DateTimeField(auto_now_add=True)
+  location = models.ForeignKey(Location,on_delete=models.CASCADE, default='location')
+  category = models.ForeignKey(Category,on_delete=models.CASCADE, default='')
+  
+  def __str__(self):
+    return self.name
+  
+  class Meta:
+    ordering =['name']
   
   
