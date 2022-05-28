e04740e5280ca6a72478f6de88ddd77e5dff2832
Collins Sirwani <sircollo71@gmail.com>
2022-05-29 01:49:46 +0300

    Feat: add methods for model classes

--- gallery/models.py
+++ gallery/models.py
@@ -8,11 +8,33 @@ class Location(models.Model):
   def __str__(self):
     return self.name
   
+  def save_location(self):
+    self.save()
+    
+ 
+  @classmethod 
+  def update_location(cls, id, value):
+    cls.objects.filter(id=id).update(name=value)
+    
+  def delete_location(self):
+    self.delete()
+    
+  
 class Category(models.Model):
   name = models.CharField(max_length=30)
   
   def __str__(self):
     return self.name
+  
+  def save_category(self):
+    self.save()
+    
+  @classmethod 
+  def update_category(cls, id, value):
+    cls.objects.filter(id=id).update(name=value)
+    
+  def delete_category(self):
+    self.delete()
   
 class Image(models.Model):
   name = models.CharField(max_length=30)
@@ -22,10 +44,38 @@ class Image(models.Model):
   location = models.ForeignKey(Location,on_delete=models.CASCADE, default='location')
   category = models.ForeignKey(Category,on_delete=models.CASCADE, default='')
   
+  
   def __str__(self):
     return self.name
   
   class Meta:
     ordering =['name']
+    
+  def save_image(self):
+    self.save()
+  
+  @classmethod
+  def update_image(cls,id,value):
+    cls.objects.filter(id=id).update(picture=value)
+  
+
+  def delete_image(self):
+    self.delete()
+  
+  @classmethod
+  def get_image_by_id(cls, id):
+    image = cls.objects.filter(id=id).all()
+    return image
+  
+  @classmethod
+  def search_by_category(cls, search_term):
+      images = cls.objects.filter(category__name__icontains=search_term)
+      return images
+  
+  @classmethod
+  def filter_by_location(cls, location):
+    image_location = Image.objects.filter(location__name=location).all()
+    return image_location
+    
   
   
