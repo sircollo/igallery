ba3c3ec2c8800aa9a61de59e081ee1c2e34a683c
Collins Sirwani <sircollo71@gmail.com>
2022-05-28 22:07:42 +0300

    Feat: add image model class

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,10 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+  name = models.CharField(max_length=30)
+  description = models.TextField()
+  picture = models.ImageField(upload_to='images',default='image')
+  date = models.DateTimeField(auto_now_add=True)
