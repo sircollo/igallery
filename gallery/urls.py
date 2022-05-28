1b7f8206716efff0f8304c420515a05fa2169ed8
Collins Sirwani <sircollo71@gmail.com>
2022-05-28 22:02:00 +0300

    This will configure image uploads

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,12 @@
 from django.urls import re_path as url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns =[
   url('^$',views.home,name='index'),
 
 ]
+if settings.DEBUG:
+  urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- igallery/settings.py
+++ igallery/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -120,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
