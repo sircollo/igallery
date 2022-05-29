b88313aaee69ac021ff81e5fab335dd382501dd1
Collins Sirwani <sircollo71@gmail.com>
2022-05-30 01:19:33 +0300

    Feat: add url to get images from a location

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns =[
   url('^$',views.home,name='index'),
-  url(r'^search/', views.search_results, name='search_results')
+  url(r'^search/', views.search_results, name='search_results'),
+  url(r'^location/(\d+)',views.filter_location,name='locations'),
 ]
 if settings.DEBUG:
   urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
