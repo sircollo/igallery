2bd816c3f2845ee08d69009f86b521ee9d2c8e79
Collins Sirwani <sircollo71@gmail.com>
2022-05-29 22:00:49 +0300

    Feat: add embed video

--- igallery/settings.py
+++ igallery/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'gallery',
+    'embed_video'
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'django.template.context_processors.request',
             ],
         },
     },
