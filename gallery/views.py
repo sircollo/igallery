14abdbbf89b97fff1ad4734c1b367b8a2339d4cd
Collins Sirwani <sircollo71@gmail.com>
2022-05-30 00:52:21 +0300

    Feat: add search results function

--- gallery/views.py
+++ gallery/views.py
@@ -4,4 +4,18 @@ from .models import *
 # Create your views here.
 def home(request):
     images = Image.objects.all()
-    return render(request, 'index.html',{'images':images})
+    locations = Location.objects.all()
+    return render(request, 'index.html',{'images':images,'locations':locations})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
