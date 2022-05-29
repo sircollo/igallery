3236b3884e6bd77bffdc98bdf3f3d390eb2a9ece
Collins Sirwani <sircollo71@gmail.com>
2022-05-30 00:51:20 +0300

    Feat: add search path

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns =[
   url('^$',views.home,name='index'),
-
+  url(r'^search/', views.search_results, name='search_results')
 ]
 if settings.DEBUG:
   urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
